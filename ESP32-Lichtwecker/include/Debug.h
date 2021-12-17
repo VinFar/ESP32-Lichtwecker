120bb9a1356a99c51f6f9634845eb3353632e079
Vincent <v.fartmann@gmail.com>
2021-12-17 22:29:42 +0100

    add: debug prefixes for WEBUI

--- ESP32-Lichtwecker/include/Debug.h
+++ ESP32-Lichtwecker/include/Debug.h
@@ -20,17 +20,19 @@ void DebugPrintFunction(DebugPrefix_t DebugPrefix,
 
 #define CLI_NL "\r\n"
 
-#define DEBUG_PREFIX_MAIN	"[MAIN] "
-#define DEBUG_PREFIX_WIFI	"[WIFI] "
-#define DEBUG_PREFIX_SNTP	"[SNTP] "
-#define DEBUG_PREFIX_LED	"[LED] "
+#define DEBUG_PREFIX_MAIN		"[MAIN] "
+#define DEBUG_PREFIX_WIFI		"[WIFI] "
+#define DEBUG_PREFIX_SNTP		"[SNTP] "
+#define DEBUG_PREFIX_LED		"[LED] "
+#define DEBUG_PREFIX_WEBUI		"[WEBUI] "
 
 #define DEBUG_MSG_MAIN (DebugPrefixes[0])
 #define DEBUG_MSG_WIFI (DebugPrefixes[1])
 #define DEBUG_MSG_SNTP (DebugPrefixes[2])
 #define DEBUG_MSG_LED (DebugPrefixes[3])
+#define DEBUG_MSG_WEBUI (DebugPrefixes[4])
 
-extern DebugPrefix_t DebugPrefixes[4];
+extern DebugPrefix_t DebugPrefixes[5];
 
 
 #endif
--- ESP32-Lichtwecker/src/Debug.cpp
+++ ESP32-Lichtwecker/src/Debug.cpp
@@ -2,40 +2,46 @@
 
 #ifdef DEBUG
 
-DebugPrefix_t DebugPrefixes[4] = { { 1, DEBUG_PREFIX_MAIN },{1,DEBUG_PREFIX_WIFI},{1,DEBUG_PREFIX_SNTP}};
+DebugPrefix_t DebugPrefixes[5] = {{1, DEBUG_PREFIX_MAIN}, {1, DEBUG_PREFIX_WIFI}, {1, DEBUG_PREFIX_SNTP}, {1, DEBUG_PREFIX_LED},{1, DEBUG_PREFIX_WEBUI}};
 
 void DebugPrintFunction(DebugPrefix_t DebugPrefix,
-		const char *format, ...) {
-	if (DebugPrefix.Enabled) {
-		Serial.print(DebugPrefix.DebugPrefix);
-		    char loc_buf[64];
-    char * temp = loc_buf;
-    va_list arg;
-    va_list copy;
-    va_start(arg, format);
-    va_copy(copy, arg);
-    int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
-    va_end(copy);
-    if(len < 0) {
-        va_end(arg);
-        return;
-    };
-    if(len >= sizeof(loc_buf)){
-        temp = (char*) malloc(len+1);
-        if(temp == NULL) {
+                        const char *format, ...)
+{
+    if (DebugPrefix.Enabled)
+    {
+        Serial.print(DebugPrefix.DebugPrefix);
+        char loc_buf[64];
+        char *temp = loc_buf;
+        va_list arg;
+        va_list copy;
+        va_start(arg, format);
+        va_copy(copy, arg);
+        int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
+        va_end(copy);
+        if (len < 0)
+        {
             va_end(arg);
             return;
+        };
+        if (len >= sizeof(loc_buf))
+        {
+            temp = (char *)malloc(len + 1);
+            if (temp == NULL)
+            {
+                va_end(arg);
+                return;
+            }
+            len = vsnprintf(temp, len + 1, format, arg);
         }
-        len = vsnprintf(temp, len+1, format, arg);
-    }
-    va_end(arg);
-    len = Serial.write((uint8_t*)temp, len);
-    if(temp != loc_buf){
-        free(temp);
+        va_end(arg);
+        len = Serial.write((uint8_t *)temp, len);
+        if (temp != loc_buf)
+        {
+            free(temp);
+        }
+        return;
     }
     return;
-	}
-	return;
 }
 
 #endif
