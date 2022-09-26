f852fba2d885b312a9f40a25733beb6b03038a5b
Vincent Fartmann <v.fartmann@gmail.com>
2022-09-26 22:41:03 +0200

    add: prototypes for CLI

---
+++ ESP32-Lichtwecker/src/CLI.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "CLI.h"
+#include "SimpleCLI.h"
+#include "Husarnet.h"
+
+
+void TaskCli(void *arg);
+
+HusarnetServer server(CLI_PORT);
+SimpleCLI cli;
+
+void CliInit(){
+
+
+
+}
+
+
+void TaskCli(void *arg){
+
+
+
+}
