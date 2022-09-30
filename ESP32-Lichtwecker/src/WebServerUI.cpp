e217a38f73be6d7b7504f121db23c7df9c2b2f59
Vincent Fartmann <v.fartmann@gmail.com>
2022-09-30 10:29:45 +0200

    v1.1.0.5

--- ESP32-Lichtwecker/src/WebServerUI.cpp
+++ ESP32-Lichtwecker/src/WebServerUI.cpp
@@ -26,7 +26,7 @@ void TaskWebUI(void *arg)
   DEBUG_PRINT("DNS Status %s" CLI_NL, Status ? "OK" : "Fail");
   WebUiInitTabs();
 
-  ESPUI.begin("Lichtwäcker v1.1.0.4");
+  ESPUI.begin("Lichtwäcker v1.1.0.5");
   DEBUG_PRINT("Started WebUI" CLI_NL);
   EspUiStarted = true;
 
